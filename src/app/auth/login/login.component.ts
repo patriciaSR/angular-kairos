3b12cb707abe747a7c13b9c41b98e706ed7007a6
patriciaSR <patricia.suarez.psr@gmail.com>
2020-02-27 17:48:25 +0100

    add more forms practices

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { CommonValidator } from 'src/app/common-validator';
 import { LoginService } from '../login.service';
@@ -12,17 +12,64 @@ import { TokenDTO } from '../token.dto';
 })
 export class LoginComponent implements OnInit, OnDestroy {
   form: FormGroup;
+  optForm: FormGroup;
+  profileForm: FormGroup;
   userToken: TokenDTO;
   sub: Subscription;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       username: new FormControl('', [Validators.required, CommonValidator.startWithNumber], [CommonValidator.userTaken]),
       password: new FormControl('', [Validators.required, Validators.minLength(3)])
     });
+
+    this.optForm = new FormGroup({
+      a: new FormControl(''),
+      b: new FormControl(''),
+    });
+
+    this.optForm.get('a').valueChanges.subscribe(checked => {
+      if (checked) {
+        const validators = [Validators.required, Validators.minLength(5)];
+
+        this.optForm.addControl('opcionExtra', new FormControl('', validators));
+      } else {
+        this.optForm.removeControl('opcionExtra');
+      }
+      this.optForm.updateValueAndValidity();
+    });
+
+    this.profileForm = this.fb.group({
+      name: ['', [Validators.required]],
+      description: [''],
+      intervals: this.fb.array([]),
+    });
   }
+
+  intervalsForms() {
+    return this.profileForm.get('intervals') as FormArray;
+  }
+
+  addInterval() {
+    const interval = this.fb.group({
+      hourType: [''],
+      hour: [''],
+      powerValue: [''],
+    });
+
+    this.intervalsForms().push(interval);
+  }
+
+  deleteInterval(i: number) {
+    this.intervalsForms().removeAt(i);
+  }
+
+  send() {
+    console.log(this.profileForm.value);
+  }
+
   ngOnDestroy(): void {
     this.sub.unsubscribe();
   }
